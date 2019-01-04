58bee91650cb746d89585ab831b4d6772224d91f
qwertpas <christopher.y.x@gmail.com>
2019-01-03 21:41:21 -0800

    first commit

---
+++ src/maybepid/Physics2.java
@@ -0,0 +1,89 @@
+package maybepid;
+
+import java.awt.MouseInfo;
+
+public class Physics2 {
+
+	//TUNE
+	final static double kP = 0.06;
+	final static double kI = 0.045;
+	final static double kD = 0.4;
+	final static double posTolerance = 2;
+	final static double velTolerance = 0.01;
+	//Goes fast, overshoots just a bit, then corrects itself. Finishes in less time and effective for long distances, but might not be realistic for motors to accelerate this quickly.
+	
+	
+	
+	static double position = 10;
+	static double velocity = 0;
+	static double acceleration = 0;
+	
+	static double efficiency = 0.95;
+	
+	static double target = 260;
+	static double error = target;
+	static double lasterror;
+	
+	static double P = 0.0;
+	static double I = 0.0;
+	static double D = 0.0;
+	
+	
+	public static void main(String[] args) {
+		
+		double startTime = System.currentTimeMillis();
+		Boolean done = false;
+
+		while(!done) {
+			
+			target = MouseInfo.getPointerInfo().getLocation().getX() * (260.0/1919.0);
+			System.out.println("target: " + target);
+			
+			lasterror = error;
+			error = target - position;
+			
+			if (Math.abs(error) < 2 ) {
+				I = 0;
+			}
+			
+			P = kP * error;
+			I = kI * (I + error);
+			D = kD * (error - lasterror);
+			
+			acceleration = P + I + D;
+			
+			velocity = (velocity + acceleration) * efficiency;
+			position = position + velocity;
+			
+			
+			
+			display((int) Math.round(position));
+			System.out.println("P: " + (double) P + "      I: " + (double) I + "     D: " + (double) D);
+			
+			if(Math.abs(velocity) < velTolerance && Math.abs(error) < posTolerance) {
+				//comment out the following line to make it keep running after it reaches the tolerances
+				done = true;
+			}
+
+			//20 millisecond delay or 50 times a second
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+		
+		System.out.println("target reached in  " + (System.currentTimeMillis() - startTime) + " milliseconds.");
+		System.out.println("ending velocity:  " + velocity);
+	}
+	
+	public static void display(int position) {
+		System.out.print("||");
+		for(int i = 0; i < position; i++) {
+			System.out.print(" ");
+		}
+		System.out.println("I");
+	}
+
+}
